043bc996dc1ef74605b9b7b5c7159dfdd7702e07
Elispher <elispher@Elisphers-MacBook-Air.local>
2026-01-12 00:39:45 -0500

    Update hello script with input handling

--- scripts/hello.py
+++ scripts/hello.py
@@ -1,6 +1,12 @@
-print("First pyhton script")
-#Mfirst python variables
-name ="ellie"
-age=27
-print(("Name:",name))
-print("Age:",age)
+# Day 3 - Safe decision making
+
+try:
+    age = int(input("Enter Your Age."))
+
+    if age >= 18:
+        print("Access granted. You are an adult.")
+    else:
+        print("Access denied. You are under 18.")
+
+except ValueError:
+    print("Invalid input. Please enter a number.")
